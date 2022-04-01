e987e2f4db2f271295264c97c8ddb265d83ab10f
markmulvenna <markmulvenna123@gmail.com>
2022-04-01 14:17:15 -0700

    Github

---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Todo;
+
+Route::get('/', [Todo::class, 'index']);
+Route::post('/todos', [Todo::class, 'store']);
+Route::put('/todos/{id}', [Todo::class, 'update']);
+Route::delete('/todos/{id}', [Todo::class, 'destroy']);
